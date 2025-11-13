809e831bc41010590aa6156e480d5bbf543c657c
calogerosorce <calogerosorce00@hotmail.com>
2025-11-13 13:16:47 +0100

    Add function for onChance and function for onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
 
 
 function App() {
+  const api = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    check: 'pubblico'
+  })
+  function handleFormdData(e) {
+    const value =
+      e.target.name === e.target.value ?
+        e.target.checked : e.target.value;
+    setFormData({
+      ...formData,
+      [e.target.name]: value,
+    });
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
   return (
     <>
@@ -10,30 +30,34 @@ function App() {
           <h1>INVIA I TUOI DATI</h1>
         </header>
         <main>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-3">
               <label className="form-label">Author</label>
-              <input type="text" className="form-control" />
+              <input type="text" name='author' className="form-control" value={formData.author} onChange={handleFormdData} />
             </div>
             <div className="mb-3">
               <label className="form-label">Title</label>
-              <input type="text" className="form-control" />
+              <input type="text" name='title' className="form-control" value={formData.title} onChange={handleFormdData} />
             </div>
             <div className="mb-3">
               <label className="form-label">Body</label>
-              <textarea className="form-control" rows="3"></textarea>
+              <input type="text" name='body' className="form-control" value={formData.body} onChange={handleFormdData} />
             </div>
             <div className="row d-flex justify-content-center">
               <div className="col-2">
-                <div className="mb-3 form-check">
-                  <input type="checkbox" className="form-check-input" />
-                  <label className="form-check-label">Pubblico</label>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="check" value='pubblico' checked={formData.check === 'pubblico'} onChange={handleFormdData} />
+                  <label className="form-check-label">
+                    Pubblico
+                  </label>
                 </div>
               </div>
               <div className="col-2">
-                <div className="mb-3 form-check">
-                  <input type="checkbox" className="form-check-input" />
-                  <label className="form-check-label">Bozza</label>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="check" value='bozza' checked={formData.check === 'bozza'} onChange={handleFormdData} />
+                  <label className="form-check-label">
+                    Bozza
+                  </label>
                 </div>
               </div>
             </div>
